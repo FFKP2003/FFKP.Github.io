4e8a0e3312ee27f0ec9a7574e532bd0a27521fdc
unknown <faqih.213040033@mail.unpas.ac.id>
2021-12-26 22:50:20 +0700

    Portfolio faqih

---
+++ js/script.js
@@ -0,0 +1,13 @@
+@keyframes animate {
+    0%{
+        transform: rotateY(90deg);
+    }
+    10%,100% {
+        transform: rotateY(270deg);
+    }
+}
+@media (max-width:768px) {
+    .container2 .popup-video video {
+        width: 95%;
+    }
+}
