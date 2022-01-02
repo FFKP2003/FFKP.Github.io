6f33add04528d061b851dfee0a13cf09d8923c0c
FFKP2003 <faqih.213040033@mail.unpas.ac.id>
2022-01-02 11:49:09 +0700

    update.css and js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-document.querySelectorAll('.video-container2 video').forEach(vid => {
-    vid.onclik = () => {
-        document.querySelector('.popup-video').style.display = 'block';
-        document.querySelector('.popup-video video').src = vid.getAttribute('src');
-    }
+document.querySelectorAll(".video-container2 video").forEach((vid) => {
+  vid.onclik = () => {
+    document.querySelector(".popup-video").style.display = "block";
+    document.querySelector(".popup-video video").src = vid.getAttribute("src");
+  };
 });
 
-document.querySelector('.popup-video span').onclick = () => {
-    document.querySelector('.popup-video').style.display = 'none';
-}
+document.querySelector(".popup-video span").onclick = () => {
+  document.querySelector(".popup-video").style.display = "none";
+};
 hljs.initHighlightingOnLoad();
-        
-  $('.hero__scroll').on('click', function(e) {
-  $('html, body').animate({
-        scrollTop: $(window).height()
-       }, 1200);
- });      
-       AOS.init({
-       easing: 'ease-out-back',
-       duration: 1000
- });
-                   
+
+$(".hero__scroll").on("click", function (e) {
+  $("html, body").animate(
+    {
+      scrollTop: $(window).height(),
+    },
+    1200
+  );
+});
+AOS.init({
+  easing: "ease-out-back",
+  duration: 1000,
+});
