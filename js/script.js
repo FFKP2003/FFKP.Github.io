3284e01dab1d2c0467cc1c69b055c44a473a5fa7
FFKP2003 <faqih.213040033@mail.unpas.ac.id>
2022-01-04 01:24:04 +0700

    SPIDERMAN

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Owl-carousel
+
+$(".site-main .about-area .owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    544: {
+      items: 2
+    },
+  },
+})
+
+
+
+});
