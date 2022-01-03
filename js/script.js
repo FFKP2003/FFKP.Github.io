bf170e8fcd25ac92501e692bbe1c731190fea01a
faqih.213040033@mail.unpas.ac.id <faqih.213040033@mail.unpas.ac.id>
2022-01-04 06:31:51 +0700

    tolong ubah

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 // Owl-carousel
-
 $(".site-main .about-area .owl-carousel").owlCarousel({
   loop: true,
   autoplay: true,
   dots: true,
   responsive: {
     0: {
-      items: 1
+      items: 1,
     },
     544: {
-      items: 2
+      items: 2,
     },
   },
-})
-
-
-
 });
